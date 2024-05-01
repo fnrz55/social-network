b469387b4c2f31e0f27acaa216fed81cdaf4a1f8
fnrz55 <dff3300@gmail.com>
2024-05-01 23:08:48 +0300

    Updates

--- src/utilits/validators/objectHelpers.js
+++
@@ -1,8 +0,0 @@
-export const updateObjInArray=(items,itemId,objPropName,newObjprops)=>{
-    return items.map(u => {
-        if (u[objPropName] === itemId) {
-            return{...u,...newObjprops}
-        }
-        return u
-    })
-}
